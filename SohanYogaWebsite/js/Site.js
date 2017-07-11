73047a34a16f714e13b5db2dd280ac6e31d7cfef
mohanmahale <mohan@idpsolutions.com.au>
2017-07-11 20:51:04 +0530

    Add project files.

---
+++ SohanYogaWebsite/js/Site.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    var headerHeight = $('nav').outerHeight();
+    $('.slide-section').click(function (e) {
+        var linkHref = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top - headerHeight
+        }, 1000);
+        e.preventDefault();
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 150) {
+            $("#back-top").addClass("visible");
+        } else {
+            $("#back-top").removeClass("visible");
+        }
+    });
+});
